496a5463e41b8094e8fa8f020857ab44f3bcb016
Alex Uldahl Pedersen <alex@uldahl.dk>
2025-02-18 13:34:13 +0100

    Folder renaming

--- ExerciseA/User.cs
+++
@@ -1,9 +0,0 @@
-namespace ExerciseA;
-
-public class User
-{
-    public int Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public int Age { get; set; }
-}
